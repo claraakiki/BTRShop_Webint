8519238d881e2957fd2be982dc6adb52d753c46c
lamia <lamia.salhi@eurecom.fr>
2020-12-29 09:42:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let toggleBtn = document.getElementById('menu-toggle');
+let sidebarWrapper = document.getElementById('wrapper');
+function main() {
+    // check whether we are logged in or not
+
+}
+
+function toggleDropdown(){
+    var menu = document.getElementById("dropdown");
+    if (menu.style.display == "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+  
